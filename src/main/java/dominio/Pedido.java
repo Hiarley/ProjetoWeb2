9be8e73a3df3630f9b95cf07b6469efda66e7ef9
Hiarley Farias <hiarley.farias@gmail.com>
2016-10-08 20:19:39 -0300

    referencia pedido com usuario.

--- src/main/java/dominio/Pedido.java
+++ src/main/java/dominio/Pedido.java
@@ -8,118 +8,123 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "pedido")
 public class Pedido {
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "id_Pedido", nullable = false)
-    private long id;
-	
+	private long id;
+
+	@ManyToOne
+	@JoinColumn(name = "id_usuario", referencedColumnName = "id_usuario")
+	private Usuario usuarioSolicitante;
+
 	@Column(name = "dataAbertura", nullable = false)
 	private Date dataAbertura;
-	
 
-	
-	@Column(name= "descricao", nullable = false)
-    private String descricao;
-	
-	@Column(name= "status", nullable = false)
-    private char status;
-	
-	
-    private ArrayList<Produto> listaProdutos;
+	@Column(name = "descricao", nullable = false)
+	private String descricao;
 
-    public Pedido() {
-    }
+	@Column(name = "status", nullable = false)
+	private char status;
 
-    public Pedido(Usuario usuarioSolicitante, Date dataAbertura,
-             String descricao, char status,
-            ArrayList<Produto> listaProdutos) {
+	private ArrayList<Produto> listaProdutos;
 
-        this.dataAbertura = dataAbertura;
-        this.descricao = descricao;
-        this.status = status;
-        this.listaProdutos = listaProdutos;
-    }
+	public Pedido() {
+	}
 
+	public Pedido(Usuario usuarioSolicitante, Date dataAbertura, String descricao, char status,
+			ArrayList<Produto> listaProdutos) {
 
+		this.dataAbertura = dataAbertura;
+		this.descricao = descricao;
+		this.status = status;
+		this.listaProdutos = listaProdutos;
+	}
 
+	/**
+	 * @return the idServico
+	 */
+	public long getIdServico() {
+		return id;
+	}
 
-    /**
-     * @return the idServico
-     */
-    public long getIdServico() {
-        return id;
-    }
+	/**
+	 * @param idServico
+	 *            the idServico to set
+	 */
+	public void setIdServico(long idPedido) {
+		this.id = idPedido;
+	}
 
-    /**
-     * @param idServico the idServico to set
-     */
-    public void setIdServico(long idPedido) {
-        this.id = idPedido;
-    }
+	/**
+	 * @return the dataAbertura
+	 */
+	public Date getDataAbertura() {
+		return dataAbertura;
+	}
 
-    /**
-     * @return the dataAbertura
-     */
-    public Date getDataAbertura() {
-        return dataAbertura;
-    }
+	/**
+	 * @param dataAbertura
+	 *            the dataAbertura to set
+	 */
+	public void setDataAbertura(Date dataAbertura) {
+		this.dataAbertura = dataAbertura;
+	}
 
-    /**
-     * @param dataAbertura the dataAbertura to set
-     */
-    public void setDataAbertura(Date dataAbertura) {
-        this.dataAbertura = dataAbertura;
-    }
+	/**
+	 * @return the descricao
+	 */
+	public String getDescricao() {
+		return descricao;
+	}
 
-    /**
-     * @return the descricao
-     */
-    public String getDescricao() {
-        return descricao;
-    }
+	/**
+	 * @param descricao
+	 *            the descricao to set
+	 */
+	public void setDescricao(String descricao) {
 
-    /**
-     * @param descricao the descricao to set
-     */
-    public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
 
-        this.descricao = descricao;
-    }
+	/**
+	 * @return the status
+	 */
+	public char getStatus() {
 
-    /**
-     * @return the status
-     */
-    public char getStatus() {
+		return status;
+	}
 
-        return status;
-    }
+	/**
+	 * @param status
+	 *            the status to set
+	 */
+	public void setStatus(char status) {
 
-    /**
-     * @param status the status to set
-     */
-    public void setStatus(char status) {
+		this.status = status;
+	}
 
-        this.status = status;
-    }
+	/**
+	 * @return the listaProdutos
+	 */
+	public ArrayList<Produto> getListaProdutos() {
+		return listaProdutos;
+	}
 
-    /**
-     * @return the listaProdutos
-     */
-    public ArrayList<Produto> getListaProdutos() {
-        return listaProdutos;
-    }
+	/**
+	 * @param listaProdutos
+	 *            the listaProdutos to set
+	 */
+	public void setListaProdutos(ArrayList<Produto> listaProdutos) {
 
-    /**
-     * @param listaProdutos the listaProdutos to set
-     */
-    public void setListaProdutos(ArrayList<Produto> listaProdutos){
-
-        this.listaProdutos = listaProdutos;
-    }
+		this.listaProdutos = listaProdutos;
+	}
 
 }
