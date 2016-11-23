4bad0a4c1bbcb004191aaf2d6623dc491f6194e8
Thiagolink <t.t.2007@hotmail.com>
2016-11-23 12:25:18 -0300

    Add files via upload

---
+++ src/main/java/dominio/UsuarioFacebook.java
@@ -0,0 +1,22 @@
+package dominio;
+
+import facebook4j.Facebook;
+
+public class UsuarioFacebook {
+	
+		private static final long serialVersionUID = -4479049427907244695L;
+		private Facebook facebook;
+
+		public Facebook getFacebook()
+		{
+			return facebook;
+		}
+
+		public void setFacebook(Facebook facebook)
+		{
+			this.facebook = facebook;
+		}
+
+		
+	}
+ 
